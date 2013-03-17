ae374c6cf7c1150972a3da6f28453bdfb03352c1
chedim <dima@zenmoney.ru>
2013-03-17 10:22:27 +0400

    ;)

--- src/ru/zenmoney/library/dal/DbInit.java
+++ src/ru/zenmoney/library/dal/DbInit.java
@@ -9,8 +9,6 @@ package ru.zenmoney.library.dal;
  */
 public class DbInit {
     public static final String[] sql = {
-            "CREATE TABLE `sms_format` (id INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, company INTEGER, `check` TEXT, `regexp` TEXT, `columns` TEXT);",
-            "create table sms_table (_id integer primary key autoincrement,sender text, text text, time_stamp integer, status integer, `transaction` integer, parsed INTEGER);",
             "create table account (_id integer primary key autoincrement,id INTEGER, archive INTEGER, balance numeric(20,4), bank INTEGER, capitalization INTEGER, card_system INTEGER, created TEXT, date_limit TEXT, date_limit_interval INTEGER, in_balance INTEGER, instrument INTEGER, payoff_period INTEGER, payoff_type INTEGER, percent REAL, static_id INTEGER, sum numeric(20,4), title TEXT collate nocase, type TEXT, user int, connection int, changed integer DEFAULT CURRENT_TIMESTAMP);",
             "create table `transaction` (_id integer primary key autoincrement,id integer,static_id integer,account_outcome integer,account_income integer,user integer,income numeric(20,4),outcome numeric(20,4),instrument_income integer,instrument_outcome integer,category integer,payee text,comment text,price real,date text,changed integer DEFAULT CURRENT_TIMESTAMP);",
             "CREATE TABLE instrument (id INTEGER PRIMARY KEY, title TEXT collate nocase, title_short TEXT, symbol TEXT, rub_rate rate REAL);",
@@ -25,7 +23,6 @@ public class DbInit {
             "CREATE TABLE connection_data (_id INTEGER PRIMARY KEY, id integer, user integer, connection integer, name text, value text, account integer, active integer, error_text text, changed integer DEFAULT CURRENT_TIMESTAMP);",
             "INSERT INTO account (balance, sum, title, type, static_id, instrument) VALUES (0, 0, 'Наличные', 'cash', 18, 2);",
             "INSERT INTO account (balance, sum, title, type, static_id, instrument) VALUES (0, 0, 'Долги', 'debt', 3, 2);",
-            "CREATE TABLE phone (_id INTEGER PRIMARY KEY, company INTEGER, number TEXT);",
             // ^^ 15th element, version 1
             "CREATE TABLE deletion (object STRING, object_id LONG, stamp LONG DEFAULT CURRENT_TIMESTAMP);",
             // ^^ 16th element, version 2
